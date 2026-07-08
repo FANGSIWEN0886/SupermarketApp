ff2d122dca3174e8735de66b9fec21cfcf6e81bf
FANGSIWEN0886 <25028592@myrp.edu.sg>
2026-07-08 10:05:35 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+
+// Create MySQL connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'RP738964$',
+    database: 'c237_supermarketapp'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Set up view engine
+app.set('view engine', 'ejs');
+
+// enable static files
+app.use(express.static('public'));
+
+//Middleware to parse incoming request bodies
+app.use(express.urlencoded({
+    extended: false
+}));
+
+// Define routes
+app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM products';
+    // Fetch data from MySQL
+    connection.query( sql , (error, results) => {
+    if (error) {
+        console.error('Database query error:', error.message);
+        return res.send('Error Retrieving products');
+    }
+    // Render HTML page with data
+    //products is the key used in ejs file, while results stands for the raw data. 
+    res.render('index', { products: results });
+    });
+});
+
+//Retrieve one product with its ID
+app.get('/product/:ID', (req, res) => {
+    // Extract the product ID from the request parameters
+    const productId = req.params.ID;
+    const sql = 'SELECT * FROM products WHERE productId = ?';
+    // Fetch data from MySQL based on the product ID
+    connection.query( sql , [productId], (error, results) => {
+        //Catching DB issues.
+        if (error) {
+            console.error('Database query error:', error.message);
+            return res.send('Error Retrieving product by ID');
+        }
+        // Check if any product with the given ID was found(product side):
+        if (results.length > 0) {
+            // Render HTML page with the product data
+            res.render('product', { product: results[0] });
+        } else {
+            // If no product with the given ID was found
+            res.send('Product not found');
+        }
+    });
+});
+
+// Route to display the form for adding a new product
+app.get('/addProduct', (req, res) => {
+    res.render('addProduct');
+});
+// Route to handle the form submission for adding a new product
+app.post('/addProduct', (req, res) => {
+    // Extract product data from the request body
+    const { name, quantity, price, image } = req.body;
+    const sql = 'INSERT INTO products (productName, quantity, price, image) VALUES (?, ?, ?, ?)';
+    // Insert the new product into the database
+    connection.query( sql , [name, quantity, price, image], (error, results) => {
+        if (error) {
+            // Handle any error that occurs during the database operation
+            console.error("Error adding product:", error);
+            // Show error message to client side webpage.
+            res.send('Error adding product');
+        } else {
+            // Send a success response
+            // Using redirect instead of render
+            res.redirect('/');
+        }
+    });
+});
+
+// Route to display the form for editing a product
+app.get('/editProduct/:ID', (req, res) =>{
+    const productId = req.params.ID;
+    const sql = "SELECT * FROM products WHERE productId = ?";
+    //Fetch data from Mysql based on the product ID
+    connection.query(sql, [productId], (error, results) => {
+        if (error) {
+            console.error("Database query error:", error.message);
+            return res.send("Error retrieving poduct by ID");
+        };
+        //Check if any product with the given ID was found 
+        //Checking part will be done in the get request
+        if (results.length > 0){
+            //Render HTML page with the product data
+            res.render("editProduct", {product:results[0]});
+        } else {
+            res.send("Product not found");
+        };
+    });
+});
+
+// Route to handle the form submission for editing a product
+app.post('/editProduct/:ID', (req, res) => {
+    const productId = req.params.ID;
+    const {name, quantity, price} = req.body;
+    const sql = "UPDATE products SET productName = ?, quantity = ?, price = ? WHERE productId = ?";
+
+    //Edit the existing product in DB
+    connection.query(sql, [name, quantity, price, productId], (error, results) => {
+        if (error) {
+            //Handle any error that occurs during the database operation
+            console.error("Error updating product:", error);
+            res.send("Error updating product");
+        } else {
+            //Back to the main page for verifying the changes
+            res.redirect('/');
+        };
+    });
+});
+
+// Route to handle the deletion of a product
+app.get('/deleteProduct/:ID', (req, res) =>{
+    const productId = req.params.ID;
+    const sql = "DELETE FROM products WHERE productId = ?";
+    connection.query(sql, [productId], (error, results) => {
+        if (error) {
+            console.error("Error deleting product:", error);
+            res.send("Error deleting product");
+        } else {
+            res.redirect('/');
+        };
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
